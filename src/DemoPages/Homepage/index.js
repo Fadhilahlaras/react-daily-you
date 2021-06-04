77675672cf4a9cde8695155e9d195cdafd47c5e0
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-04 16:26:33 +0700

    Update

--- src/DemoPages/Homepage/index.js
+++ src/DemoPages/Homepage/index.js
@@ -1,26 +1,26 @@
-import React,{Fragment, useState, useEffect} from "react";
+import React,{Fragment} from "react";
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 import AppHeader from "../../Layout/AppHeader/";
-import AppSidebar from "../../Layout/AppSidebar/";
 import AppFooter from "../../Layout/AppFooter";
 
 import {Row, Container, Col} from "reactstrap";
 
-import CardNya from "./CardIsi";
-import Index from "./CardData";
+// import CardNya from "./CardIsi";
 import CardSlide from "./CardSlide";
 import axios from "axios";
 
 const Homepage = () => {
-    const [Index, setDataCard] = useState([])
-    let imageArrayPath = [];
 
-    useEffect(() => {
-        axios.get("http://localhost:1221/input").then(res => {
-            setDataCard(res.data)
-        })
-    }, [])
+    // const [Index, setDataCard] = useState([])
+    // let imageArrayPath = [];
+
+    //melakukan efek samping (side effects) didalam function component
+    // useEffect(() => {
+    //     axios.get("http://localhost:1221/input").then(res => {
+    //         setDataCard(res.data)
+    //     })
+    // }, [])
 
     return(
         <Fragment>
@@ -35,15 +35,15 @@ const Homepage = () => {
 
                 <div className="app-main">
                         <div className="app-main__inner">
-                            <Row>
-                                {/*<CardSlide />*/}
+                            {/*<Row>*/}
+                                <CardSlide />
 
-                            </Row>
-                            <Row>
-                                {Index.map((x, index) => (
-                                    <CardNya key={index} firstname={x.firstname} lastname={x.lastname} image={x.image} date={x.date} address={x.address} id={x.id}/>
-                                ))}
-                            </Row>
+                            {/*</Row>*/}
+                            {/*<Row>*/}
+                            {/*    {Index.map((x, index) => (*/}
+                            {/*        <CardNya key={index} firstname={x.firstname} lastname={x.lastname} image={x.image} date={x.date} address={x.address} id={x.id}/>*/}
+                            {/*    ))}*/}
+                            {/*</Row>*/}
                         </div>
 
                     <AppFooter/>
