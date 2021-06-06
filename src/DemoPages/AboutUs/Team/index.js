32b0f83a6c8f7a185692ab5ce76fe0c7d39e7068
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-06 19:29:21 +0700

    Add new page for team

--- src/DemoPages/AboutUs/Team/index.js
+++ src/DemoPages/AboutUs/Team/index.js
@@ -1,37 +1,57 @@
-import React, {useEffect, useState} from "react";
+import React, {Fragment, useState, useEffect} from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import axios from "axios";
+
+import AppHeader from "../../../Layout/AppHeader";
+import AppFooter from "../../../Layout/AppFooter";
+
+import CardTeam from "./CardData";
+
+// Examples
+
 import {
-    Button,
-    Card,
-    CardBody,
-    CardTitle, Col,
-    Form,
-    FormGroup, FormText, Input, Label,
-    Modal,
-    ModalBody,
-    ModalFooter,
-    ModalHeader, Row
+    Container,
+    Row
 } from "reactstrap";
 
+const Team = () => {
+    const [dataCard, setDataCard] = useState([])
+    let imageArrayPath = [];
 
-const ModalTeam = (props) => {
+    useEffect(() => {
+        axios.get("http://localhost:1221/team").then(res => {
+            setDataCard(res.data)
+        })
+    }, [])
 
-    // useEffect(() => {
-    //     getOptions()
-    // })
+        return (
+            <Fragment>
+                <CSSTransitionGroup
+                    component="div"
+                    transitionName="TabsAnimation"
+                    transitionAppear={true}
+                    transitionAppearTimeout={0}
+                    transitionEnter={false}
+                    transitionLeave={false}>
 
-    return (
-        <span className="d-inline-block mb-2 mr-2">
-            <Modal isOpen={props.modal} toggle={props.toggle} className={props.className}>
-                <ModalHeader toggle={props.toggle}>Misi Daily You</ModalHeader>
-                <ModalBody>
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="link" onClick={props.toggle}>Close</Button>
-                </ModalFooter>
-                </Modal>
-        </span>
-    )
+                    <AppHeader/>
 
+                    <div className="app-main">
+                        <div className="app-main__inner">
+                            <Container fluid>
+                                <Row>
+                                    {dataCard.map((x, index) => (
+                                        <CardTeam key={index} name={x.name} address={x.address} pbirth={x.pbirth}
+                                                 bdate={x.bdate} religion={x.religion} gender={x.gender} email={x.email} note={x.note} id={x.id}/>
+                                    ))}
+                                </Row>
+
+                            </Container>
+                        </div>
+                        <AppFooter/>
+                    </div>
+                </CSSTransitionGroup>
+            </Fragment>);
 }
 
-export default ModalTeam;
+export default Team;
