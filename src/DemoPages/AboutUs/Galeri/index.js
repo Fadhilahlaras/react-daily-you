1315d92aa7a2b83a3e31edeadbff170124b0ba83
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-07 15:03:39 +0700

    Add Galeri Company

--- src/DemoPages/AboutUs/Galeri/index.js
+++ src/DemoPages/AboutUs/Galeri/index.js
@@ -9,16 +9,25 @@ import AppFooter from "../../../Layout/AppFooter";
 // Examples
 
 import {
-    Card, CardTitle,
+    Card, CardTitle, CardFooter, Button,
     Col,
     Container, FormText,
     Row
 } from "reactstrap";
-import bg1 from "../../../assets/utils/images/dropdown-header/abstract1.jpg";
-import bg2 from "../../../assets/utils/images/dropdown-header/abstract2.jpg";
+import prodemy1 from "../../../assets/utils/images/prodemy/prodemy4_1.jpg";
+import prodemy2 from "../../../assets/utils/images/prodemy/prodemy4_2.jpg";
+import prodemy3 from "../../../assets/utils/images/prodemy/prodemy4_3.jpg";
+import prodemy4 from "../../../assets/utils/images/prodemy/prodemy4_4.jpg";
+import prodemy5 from "../../../assets/utils/images/prodemy/prodemy4_5.jpg";
+import prodemy6 from "../../../assets/utils/images/prodemy/prodemy4_6.jpg";
+import prodemy7 from "../../../assets/utils/images/prodemy/prodemy4_7.jpg";
+import prodemy8 from "../../../assets/utils/images/prodemy/prodemy4_8.jpg";
+import prodemy9 from "../../../assets/utils/images/prodemy/prodemy4_9.jpg";
+import prodemy10 from "../../../assets/utils/images/prodemy/prodemy4_10.jpg";
+
+
 import {Link} from "react-router-dom";
-import bg3 from "../../../assets/utils/images/dropdown-header/abstract6.jpg";
-import bg4 from "../../../assets/utils/images/dropdown-header/abstract5.jpg";
+
 
 class Galeri extends Component {
 
@@ -58,31 +67,83 @@ class Galeri extends Component {
                             <Container fluid>
                                 <h4 style={{paddingBottom: 20}}> Galeri </h4>
                                 <Row>
-                                    <Col md="4">
-                                        <Card body className="card-shadow-primary border mb-3" outline color="primary">
-                                            <div
-                                                className="h-100 d-flex justify-content-center align-items-center bg-sunny-morning">
-                                                <div className="slide-img-bg opacity-6"
-                                                     style={{
-                                                         backgroundImage: 'url(' + bg2 + ')'
-                                                     }}/>
-                                            </div>
-                                            <CardTitle>Primary Card Shadow</CardTitle>
-                                            With supporting text below as a natural lead-in to additional
-                                            content.
-
-                                        </Card>
-                                        <Card body className="card-shadow-secondary border mb-3" outline color="secondary">
-                                            <CardTitle>Secondary Card Shadow</CardTitle>
-                                            With supporting text below as a natural lead-in to additional
-                                            content.
+                                    <Col sm="12" md="6" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3"
+                                              color="primary">
+                                            <img src={prodemy1}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
                                         </Card>
                                     </Col>
-                                    <Col md="4">
-                                        <Card body className="card-border mb-3" outline color="primary">
-                                            <CardTitle>Primary Card Border</CardTitle>
-                                            With supporting text below as a natural lead-in to additional
-                                            content.
+                                    <Col sm="12" md="6" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3" color="focus">
+                                            <img src={prodemy2}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                    <Col sm="12" md="12" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3" color="dark">
+                                            <img src={prodemy3}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col sm="12" md="6" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3"
+                                              color="primary">
+                                            <img src={prodemy1}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                    <Col sm="12" md="6" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3" color="focus">
+                                            <img src={prodemy2}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                    <Col sm="12" md="12" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3" color="dark">
+                                            <img src={prodemy3}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col sm="12" md="6" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3"
+                                              color="primary">
+                                            <img src={prodemy1}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                    <Col sm="12" md="6" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3" color="focus">
+                                            <img src={prodemy2}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
+                                        </Card>
+                                    </Col>
+                                    <Col sm="12" md="12" xl="4">
+                                        <Card className="card-shadow-primary card-border text-white mb-3" color="dark">
+                                            <img src={prodemy3}/>
+                                            <CardFooter className="text-center d-block">
+                                                <h6 color="dark">Send Message</h6>
+                                            </CardFooter>
                                         </Card>
                                     </Col>
                                 </Row>
