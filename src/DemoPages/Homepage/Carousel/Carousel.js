0a057d8ada462bc56f0ec542bac94c2d316c9247
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-12 15:41:54 +0700

    Add new carousel

--- src/DemoPages/Homepage/Carousel/Carousel.js
+++ src/DemoPages/Homepage/Carousel/Carousel.js
@@ -2,9 +2,9 @@ import React, {} from 'react';
 import {
     UncontrolledCarousel
 } from 'reactstrap';
- import image1 from '../../../assets/utils/images/dropdown-header/abstract1.jpg';
- import image2 from '../../../assets/utils/images/1596721530.jpg';
- import image3 from '../../../assets/utils/images/dropdown-header/abstract3.jpg';
+ import image1 from '../../../assets/utils/images/product/makeup.jpeg';
+ import image2 from '../../../assets/utils/images/product/skincare.jpg';
+ import image3 from '../../../assets/utils/images/product/bodycare.jpg';
  
 const items = [
     {
