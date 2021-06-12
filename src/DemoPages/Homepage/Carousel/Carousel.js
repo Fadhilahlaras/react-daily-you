e9e79f3ac9f0e2f7410efa0b870b0c7ef577726f
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-13 00:58:51 +0700

    Edit picture carousel

--- src/DemoPages/Homepage/Carousel/Carousel.js
+++ src/DemoPages/Homepage/Carousel/Carousel.js
@@ -1,10 +1,12 @@
 import React, {} from 'react';
+
 import {
     UncontrolledCarousel
 } from 'reactstrap';
+
  import image1 from '../../../assets/utils/images/product/makeup.jpeg';
- import image2 from '../../../assets/utils/images/product/skincare.jpg';
- import image3 from '../../../assets/utils/images/product/bodycare.jpg';
+ import image2 from '../../../assets/utils/images/product/skincare2.jpg';
+ import image3 from '../../../assets/utils/images/product/bodycare2.jpg';
  
 const items = [
     {
