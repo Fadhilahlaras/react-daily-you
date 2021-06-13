910e88cf67fbc3b31cf8266d80ef56fb8e2db2a7
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-13 22:41:20 +0700

    update edit, delete table (not fixed)

--- src/DemoPages/AboutUs/Team/Table/edit.js
+++ src/DemoPages/AboutUs/Team/Table/edit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
     Button,
     CustomInput, Form,
@@ -12,60 +12,95 @@ import {
     Table
 } from "reactstrap";
 import {IoIosCart, IoIosSettings} from "react-icons/io";
+import axios from "axios";
 
 
 const EditMember = (props) => {
+    const [name, setName] = useState(null)
+    const [address, setAddress] = useState(null)
+    const [pbirth, setPbirth] = useState(null)
+    const [bdate, setBdate] = useState(null)
+    const [religion, setReligion] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [note, setNote] = useState(null)
+    const [file, setFile] = useState(null)
+
+    const onSubmit = (e) => {
+        const formData = new FormData();
+        const json = JSON.stringify({
+            "id": props.data.id,
+            "name": name == null ? props.data.name : name,
+            "address": address == null ? props.data.address : address,
+            "pbirth": pbirth == null ? props.data.pbirth : pbirth,
+            "bdate": bdate == null ? props.data.bdate : bdate,
+            "religion": religion == null ? props.data.religion : religion,
+            "email": email == null ? props.data.email : email,
+            "note": note == null ? props.data.note : note
+        });
+        const blobDoc = new Blob([json], {
+            type: 'application/json'
+        });
+
+        formData.append("file", file)
+        formData.append('data', blobDoc)
+        const config = {
+            headers: {
+                'content-type': 'multipart/mixed'
+            }
+        }
+        // axios.post("http://localhost:1221/team/save", formData)
+        //     .then(res => console.log(res.data))
+
+        props.onChangeToggle(false)
+        console.log(' SAVE >>')
+        console.log(file == null ? props.data : file)
+
+    }
+
     return (
         <>
             <span className="d-inline-block mb-2 mr-2">
                  <Modal isOpen={props.modal} toggle={props.toggle} className={props.className}>
-                        <ModalHeader toggle={props.toggle}><IoIosSettings size={20}/>Edit Admin</ModalHeader>
+                        <ModalHeader toggle={props.toggle}>Edit Admin</ModalHeader>
                         <ModalBody>
                             <Form>
                                              <FormGroup>
-                                                        <Label>Name</Label>
+                                                        <Label>Name : {props.data.name}</Label>
                                                         <Input type="text" name="name" id="name"
-                                                               placeholder="Name"/>
+                                                               placeholder="Name" value="name" onChange={(e) => {
+                                                                   setName(e.target.value)
+                                                        }}/>
                                                     </FormGroup>
 
                                                     <FormGroup>
-                                                        <Label>Address</Label>
+                                                        <Label>Address : {props.data.address}</Label>
                                                         <Input type="textarea" name="address" id="address"
-                                                               placeholder="Address" />
+                                                               placeholder="Address"  onChange={(e) => {
+                                                            setAddress(e.target.value)
+                                                        }}/>
                                                     </FormGroup>
 
                                                     <FormGroup>
-                                                        <Label>Place of Birth</Label>
+                                                        <Label>Place of Birth : {props.data.pbirth}</Label>
                                                         <Input type="text" name="pbirth" id="pbirth"
-                                                               placeholder="Place of Birth"
-                                                               />
+                                                               placeholder="Place of Birth" onChange={(e) => {
+                                                            setPbirth(e.target.value)
+                                                        }}/>
                                                     </FormGroup>
 
                                                     <FormGroup>
-                                                        <Label>Date of Birth</Label>
-                                                        <Input type="date" name="bdate" id="bdate"/>
+                                                        <Label>Date of Birth : {props.data.bdate}</Label>
+                                                        <Input type="date" name="bdate" id="bdate" onChange={(e) => {
+                                                            setBdate(e.target.value)
+                                                        }}/>
                                                     </FormGroup>
 
-
-                                                    <FormGroup  >
-                                                        <label for="gender">Gender</label>
-                                                        <FormGroup check>
-                                                            <Label check>
-                                                                <Input type="radio" name="radio1"/>{' '}
-                                                                Male
-                                                            </Label>
-                                                        </FormGroup>
-                                                        <FormGroup check>
-                                                            <Label check>
-                                                                <Input type="radio" name="radio1"/>{' '}
-                                                                Female
-                                                            </Label>
-                                                        </FormGroup>
-                                                    </FormGroup>
 
                                                     <FormGroup>
-                                                        <Label for="religion">Religion</Label>
-                                                        <Input type="select" name="religion" id="religion" >
+                                                        <Label for="religion">Religion: {props.data.religion}</Label>
+                                                        <Input type="select" name="religion" id="religion" onChange={(e) => {
+                                                            setReligion(e.target.value)
+                                                        }}>
                                                             <option></option>
                                                             <option>Islam</option>
                                                             <option>Christen</option>
@@ -75,26 +110,29 @@ const EditMember = (props) => {
                                                         </Input>
                                                     </FormGroup>
                                                     <FormGroup>
-                                                        <Label for="email">Email</Label>
+                                                        <Label for="email">Email : {props.data.email}</Label>
                                                         <Input type="email" name="email" id="email"
-                                                               placeholder="Input your email" />
-                                                    </FormGroup>
-                                                    <FormGroup>
-                                                        <Label>Upload your photo</Label>
-                                                        <Input type="file" name="file" id="file"
-                                                               />
+                                                               placeholder="Input your email" onChange={(e) => {
+                                                            setEmail(e.target.value)
+                                                        }}/>
                                                     </FormGroup>
 
+
                                                     <FormGroup>
-                                                        <Label>Note</Label>
-                                                        <Input type="textarea" name="note" id="note" />
+                                                        <Label>Note : {props.data.note}</Label>
+                                                        <Input type="textarea" name="note" id="note" onChange={(e) => {
+                                                            setNote(e.target.value)
+                                                        }}/>
                                                     </FormGroup>
-                                <Button color="primary" className="mt-1">Submit</Button>
+
                                         </Form>
+                            <img src={"data:image/*;base64," + props.file} height={250} width={150}/>
                         </ModalBody>
                         <ModalFooter>
-        <Button color="link" onClick={props.toggle}>Cancel</Button>
-                            <Button color="primary" onClick={props.toggle}>Save</Button>
+        <Button color="warning" onClick={props.toggle}>Cancel</Button>
+                            <Button color="primary" onClick={() => {
+                                onSubmit()
+                            }}>Save</Button>
                         </ModalFooter>
                     </Modal>
             </span>
