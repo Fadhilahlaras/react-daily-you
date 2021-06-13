1d5c6ae6de086da66c4949ebcbb964b48b0a6efd
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-06-13 22:44:05 +0700

    add new keycloak

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import Main from './DemoPages/Main';
 import configureStore from './config/configureStore';
 import { Provider } from 'react-redux';
 
+// import Keycloak from 'keycloak-js';
+
 const store = configureStore();
 const rootElement = document.getElementById('root');
 
@@ -34,3 +36,48 @@ if (module.hot) {
 }
 serviceWorker.unregister();
 
+
+// let initOptions = {
+//     url: 'https://siska.prosigmaka.com/auth', client: 'gen4-client', realm: 'gen4', onLoad: 'login-required'
+// }
+//
+//
+// let keycloak = Keycloak(initOptions);
+//
+// keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+//
+//     if (!auth) {
+//         window.location.reload();
+//     } else {
+//         console.info("Authenticated");
+//     }
+//
+//     //React Render
+//     ReactDOM.render(<Main />, document.getElementById('root'));
+//
+//     localStorage.setItem("react-token", keycloak.token);
+//     localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+//
+//     setTimeout(() => {
+//         keycloak.updateToken(70).success((refreshed) => {
+//             if (refreshed) {
+//                 console.debug('Token refreshed' + refreshed);
+//             } else {
+//                 console.warn('Token not refreshed, valid for '
+//                     + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+//             }
+//         }).error(() => {
+//             console.error('Failed to refresh token');
+//         });
+//
+//
+//     }, 60000)
+//
+// }).error(() => {
+//     console.error("Authenticated Failed");
+// });
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
